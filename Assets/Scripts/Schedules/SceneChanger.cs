ce5d07845e4519f1ac356534af691bad2611fed7
Jangbk633 <161708622+Jangbk633@users.noreply.github.com>
2024-11-14 15:20:43 +0900

    Feat: 리셋 타이틀 버튼 기능 추가
    - 팝업 캔버스 프리팹화
    - 리셋팝업과 타이틀팝업의 Yes 버튼을 누르면 TitleScene으로 이동

---
+++ Assets/Scripts/Schedules/SceneChanger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.Log("Scene 이름이 비어 있습니다.");
+        }
+    }
+}
