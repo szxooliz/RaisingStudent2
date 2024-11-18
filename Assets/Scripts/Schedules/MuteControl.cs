b82f5c99f1fee0b85d9ce0db9b9cdb51e2166da1
Jangbk633 <161708622+Jangbk633@users.noreply.github.com>
2024-11-18 11:22:27 +0900

    Chore: 오디오 mute 코드 수정
    - 자잘한 코드 수정

--- Assets/Scripts/Schedules/MuteControl.cs
+++ Assets/Scripts/Schedules/MuteControl.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MuteControl : MonoBehaviour
 {
-    public AudioSource m_AudioSource;
     public void MuteUnMuter(AudioSource m_AudioSource)
     {
         m_AudioSource.mute  = !m_AudioSource.mute;
